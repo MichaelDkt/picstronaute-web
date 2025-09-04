c6bc3e338930940f7bf98ba87b7a59ade4d591c3
MichaelDKT <mickael.gressier@gmail.com>
2025-09-04 10:35:48 +0200

    with right domaine

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-    import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
-    
-    // https://vitejs.dev/config/
-    export default defineConfig({
-      plugins: [react()],
-      base: '/picstronaute-web/', // Assurez-vous que le nom du dépôt est correct
-      envPrefix: 'VITE_', // Ajoutez cette ligne pour que Vite lise les variables d'environnement
-    })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Le chemin de base doit être la racine
+});

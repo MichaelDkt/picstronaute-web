013ae0a0ce87c5e8234ae3e007a586d692c55d1f
MichaelDKT <mickael.gressier@gmail.com>
2025-09-05 23:24:33 +0200

    Changement du JSON

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,10 @@ import './Footer.css'; // Nous créerons ce fichier plus tard
 function Footer() {
   return (
     <footer className="main-footer">
-      <p>Suivez-nous sur Instagram : <a href="https://instagram.com/picstronaute" target="_blank" rel="noopener noreferrer">@picstronaute</a></p>
+      <a href="https://instagram.com/picstronaute" target="_blank">
+      <img src="./images/logo-instagram.png" alt="Instagram Icon" className="instagram-icon" />
+      </a>
+      <p className="footer-copyright">© 2025 Picstronaute</p>
     </footer>
   );
 }

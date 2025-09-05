d470255cabb8718ccd0592b5c64650ca5493a306
MichaelDKT <mickael.gressier@gmail.com>
2025-09-05 23:46:56 +0200

    Changement du JSON

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/picstronaute-web/', // Assurez-vous que le nom du dépôt est correct
+  base: '/', 
 });

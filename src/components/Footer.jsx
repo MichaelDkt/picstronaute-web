9dd8e0c8cc9e3b7ec9055b4ef3ea9a3b89ff4897
MichaelDKT <mickael.gressier@gmail.com>
2025-10-15 19:00:41 +0200

    with conditions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './Footer.css'; // Nous créerons ce fichier plus tard
+import './Footer.css';
 
 function Footer() {
   return (
     <footer className="main-footer">
-      <a href="https://instagram.com/picstronaute" target="_blank">
-      <img src="./images/logo-instagram.png" alt="Instagram Icon" className="instagram-icon" />
+      <div className="footer-links">
+        <a href="/privacy.html">Politique de Confidentialité</a>
+        <a href="/conditions.html">Conditions d'Utilisation</a>
+      </div>
+      <a href="https://instagram.com/picstronaute" target="_blank" rel="noopener noreferrer">
+        <img src="./images/logo-instagram.png" alt="Instagram Icon" className="instagram-icon" />
       </a>
       <p className="footer-copyright">© 2025 Picstronaute</p>
     </footer>
